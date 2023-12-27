cdd40c00e874bfcc8b3d53ad35c8c20f83188036
aashish_k <aashishkathat00@gmail.com>
2023-12-28 00:51:53 +0530

    build some reusable styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+import Heading from './ui/Heading';
+import Row from './ui/Row';
+
 function App() {
   return (
-    <div>
-      hi
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <Row type="vertical">
+        <Row type="vertical">
+          <Heading as="h1">Check in and out</Heading>
+          <Button variation="primary" size="medium">
+            Check in
+          </Button>
+        </Row>
+        <Row type="horizontal">
+          <Heading as="h2">Form</Heading>
+          <form>
+            <Input type="number"></Input>
+            <Input type="number"></Input>
+          </form>
+        </Row>
+      </Row>
+    </>
+  );
 }
 
-export default App
+export default App;
