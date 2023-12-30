a7bb777cf74d052a3f22ad938537bced0e295e41
aashish_k <aashishkathat00@gmail.com>
2023-12-30 13:53:38 +0530

    fetching the cabin data

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,8 @@ import AppLayout from './ui/AppLayout';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      // staleTime: 60 * 1000,
+      staleTime: 1000,
     },
   },
 });
@@ -23,7 +24,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={true} />
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,17 +2,19 @@ import { useEffect } from 'react';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
 import { getCabins } from '../services/apiCabins';
+import CabinTable from '../features/cabins/CabinTable';
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://rahyzmexvplfykagtati.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2023-12-29T20%3A33%3A47.722Z" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
