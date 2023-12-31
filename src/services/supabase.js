f5335b405b6c80d5c24be6dfc7fea72887bc6db5
aashish_k <aashishkathat00@gmail.com>
2023-12-31 14:20:15 +0530

    Uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://rahyzmexvplfykagtati.supabase.co';
+
+export const supabaseUrl = 'https://rahyzmexvplfykagtati.supabase.co';
+
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhaHl6bWV4dnBsZnlrYWd0YXRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM4NzgyNDAsImV4cCI6MjAxOTQ1NDI0MH0.fEjr52bu6jwXgibEnYAKZGG6mWty87wULgEjR5Xj-M8';
 
