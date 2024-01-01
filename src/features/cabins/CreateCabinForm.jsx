f9a2231c8a01413d2c24494e64374ec38951345f
aashish_k <aashishkathat00@gmail.com>
2024-01-01 21:43:05 +0530

    fixed a major bug in app

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -117,7 +117,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         />
       </FormRow>
 
-      <FormRow label="Cabin photo" error={errors?.imgae?.message}>
+      <FormRow label="Cabin photo" error={errors?.image?.message}>
         <FileInput
           id="image"
           disabled={isWorking}
