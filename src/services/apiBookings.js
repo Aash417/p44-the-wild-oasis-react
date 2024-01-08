ba0c2b554f9299ad5a6f98d72ad013b4b70324eb
aashish_k <aashishkathat00@gmail.com>
2024-01-08 22:33:15 +0530

    prefetching data with react query

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,6 +1,6 @@
+import supabase from './supabase';
 import { PAGE_SIZE } from '../utils/constants';
 import { getToday } from '../utils/helpers';
-import supabase from './supabase';
 
 export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
@@ -20,8 +20,8 @@ export async function getBookings({ filter, sortBy, page }) {
     });
   // Pagination
   if (page) {
-    const from = (page - 1) * (PAGE_SIZE - 1);
-    const to = from + (PAGE_SIZE - 1);
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
     query = query.range(from, to);
   }
   const { data, error, count } = await query;
