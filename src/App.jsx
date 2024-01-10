0b0b11dcb9d4ae105fec3d59871037b0b2a838be
aashish_k <aashishkathat00@gmail.com>
2024-01-10 23:13:09 +0530

    implement authorization , protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import GlobalStyle from './styles/GlobalStyles';
 import AppLayout from './ui/AppLayout';
 import Booking from './features/bookings/Booking';
 import CheckIn from './pages/CheckIn';
+import ProtectedRoute from './ui/ProtectedRoute';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,7 +32,13 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
